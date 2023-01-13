405e206767c70cb3c39f9e207aec5b3be157b14d
bangsyir <bangsyir@gmail.com>
2023-01-13 20:20:02 +0800

    update note entity

---
+++ express-server/src/entity/Note.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Note {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: String, nullable: false })
+  title!: string;
+
+  @Column({ type: "varchar", nullable: false })
+  description!: string;
+
+  @Column()
+  views!: number;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt!: Date;
+
+  @DeleteDateColumn({ name: "deleted_at" })
+  deletedAt!: Date;
+}
