272f401783ab80a2f31f43a1bd2675d40924d383
bangsyir <bangsyir@gmail.com>
2023-01-15 17:42:00 +0800

    create user schema

---
+++ express-server/src/schema/user.ts
@@ -0,0 +1,25 @@
+const User = `#graphql
+  type User {
+    id: ID
+    name: String
+    email: String
+    password: String 
+    createdAt: String 
+    updatedAt: String 
+    deteledAt: String
+  }
+  type UserResponse {
+    status: String 
+    message: String 
+    user: User
+  }
+  type Query {
+    getUser(id: ID) : User
+  }
+  type Mutation {
+    register(name: String, email: String, password:String): User
+    login(email: String, password: String): UserResponse
+  }
+`;
+
+export default User;
