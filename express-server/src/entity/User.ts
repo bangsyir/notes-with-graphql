4c8c796175fd40190e121bc3052b8987018efd02
bangsyir <bangsyir@gmail.com>
2023-01-15 17:40:59 +0800

    create user entity

---
+++ express-server/src/entity/User.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Note } from "./Note";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: string;
+
+  @Column()
+  name!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @OneToMany(() => Note, (note) => note.user)
+  @JoinColumn({ name: "note_id" })
+  notes!: Note[];
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt!: Date;
+
+  @DeleteDateColumn({ name: "deleted_at" })
+  deletedAt!: Date;
+}
