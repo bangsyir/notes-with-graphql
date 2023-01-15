f89b85567d6500def3391bfd439e3226714aeefd
bangsyir <bangsyir@gmail.com>
2023-01-15 17:40:21 +0800

    add user resolver

--- express-server/src/index.ts
+++ express-server/src/index.ts
@@ -1,15 +1,18 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Response } from "express";
 import http from "http";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import { buildSchema } from "type-graphql";
 import bodyParser from "body-parser";
 import { conn } from "./data-source";
 import Note from "./schema/note";
 import NoteResolver from "./resolvers/noteResolver";
+import UserResolver from "./resolvers/userResolver";
+import User from "./schema/user";
+import { MyContext } from "./type";
 
 async function main() {
   // initialize database
@@ -31,8 +34,8 @@ async function main() {
   // Same ApolloServer initialization as before, plus the drain plugin
   // for our httpServer
   const server = new ApolloServer({
-    typeDefs: [Note],
-    resolvers: [NoteResolver],
+    typeDefs: [Note, User],
+    resolvers: [NoteResolver, UserResolver],
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
   // ensure we wait for our server to start
@@ -40,11 +43,16 @@ async function main() {
 
   app.use(
     "/",
-    cors<cors.CorsRequest>(),
+    cors<cors.CorsRequest>({
+      credentials: true,
+    }),
+    cookieParser(),
     bodyParser.json(),
     // expressMiddlware accept the same arguments:
     // an Apollo Server instance and optional configuration options
-    expressMiddleware(server)
+    expressMiddleware<MyContext>(server, {
+      context: async ({ req, res }) => ({ req, res, token: "" }),
+    })
   );
   // modified server startup
   await new Promise<void>((resolve) =>
