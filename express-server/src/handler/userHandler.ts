bfa2c4dd927b59c93f5d56963b7262f24ba7cf8e
bangsyir <bangsyir@gmail.com>
2023-01-22 18:18:55 +0800

    remove console.log

--- express-server/src/handler/userHandler.ts
+++ express-server/src/handler/userHandler.ts
@@ -1,9 +1,7 @@
-import { DataSource } from "typeorm";
 import { conn } from "../data-source";
 import { User } from "../entity/User";
 
 export const getUserByEmail = async (email: string) => {
-  console.log(email);
   const user = await conn
     .getRepository(User)
     .createQueryBuilder("user")
