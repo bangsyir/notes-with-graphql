af42dbaf70e4c1eb16c0be3739df28e354d9bfe8
bangsyir <bangsyir@gmail.com>
2023-01-23 18:08:36 +0800

    add getMe Query resolver

--- express-server/src/resolvers/userResolver.ts
+++ express-server/src/resolvers/userResolver.ts
@@ -1,8 +1,9 @@
 import { conn } from "../data-source";
 import { User } from "../entity/User";
-import { getUserByEmail } from "../handler/userHandler";
+import { getUserByEmail, getUserById } from "../handler/userHandler";
 import { MyContext } from "../type";
-import { hash } from "@node-rs/bcrypt";
+import { ErrorResponse } from "../handler/errorHandler";
+import { hash, verifySync } from "@node-rs/bcrypt";
 
 declare module "express-session" {
   interface SessionData {
@@ -11,6 +12,18 @@ declare module "express-session" {
 }
 
 const UserResolver = {
+  Query: {
+    async getMe(parent: any, args: any, { session }: MyContext) {
+      // get userId from session
+      const userId = session.sub as number;
+      // check user if available
+      const user = await getUserById(userId);
+      // if user not found return error
+      if (!user) ErrorResponse("User is not authenticated", 401);
+      // else return user
+      return user;
+    },
+  },
   Mutation: {
     async register(
       parent: User,
@@ -20,25 +33,60 @@ const UserResolver = {
         password: string;
       }
     ) {
+      // check email if exist
+      // if found return error else execute next line
+      const checkUser = await getUserByEmail(args.email);
+      if (!checkUser)
+        return ErrorResponse(
+          "This email already registered",
+          400,
+          "BAD_REQUEST"
+        );
+      // get password and generate to hash string
       const passwordHash = await hash(args.password, 10);
+      // initialize user
       const user = new User();
       user.name = args.name;
       user.email = args.email;
       user.password = passwordHash;
-
+      // save user to database
       await conn.manager.save(user);
+      // return user
       return user;
     },
     async login(
       parent: User,
       args: { email: string; password: string },
-      { req, res }: MyContext
+      { req, session }: MyContext
     ) {
+      // check if client has cookie session
+      const userSession = await getUserById(session.sub);
+
+      // return user hash a session
+      if (userSession) {
+        return { status: "success", message: "successfull", user: userSession };
+      }
+      // else check use from input "email"
       const user = await getUserByEmail(args.email);
-
-      if (!user) throw new Error("email or password is wrong");
-
+      // return error if not an user
+      if (!user)
+        return ErrorResponse(
+          "email or password in wrong!",
+          401,
+          "UNAUTHENTICATED"
+        );
+      // compare password input with user.password hash
+      const verifyHash = verifySync(args.password, user.password);
+      // if false return error
+      if (!verifyHash)
+        return ErrorResponse(
+          "email or password in wrong!",
+          401,
+          "UNAUTHENTICATED"
+        );
+      // create user session and send to  cookie headers
       req.session.sub = user.id;
+      // return success user
       return { status: "success", message: "successfull", user };
     },
   },
