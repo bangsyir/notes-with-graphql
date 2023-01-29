2579ba977996b15541cafb0cb594068d8709d5b3
bangsyir <bangsyir@gmail.com>
2023-01-29 18:25:54 +0800

    update error handler

--- express-server/src/handler/errorHandler.ts
+++ express-server/src/handler/errorHandler.ts
@@ -21,3 +21,30 @@ export const ErrorResponse = ({
     },
   });
 };
+
+export const validateName = (name: string) => {
+  if (name.length === 0) {
+    return "Name is require";
+  }
+  if (name.length < 5) {
+    return "Name at leats 5 characters long";
+  }
+};
+
+export const validateEmail = (Email: string) => {
+  if (Email.length === 0) {
+    return "Email is required";
+  }
+  if (Email.length < 5) {
+    return "Email at leats 5 characters long";
+  }
+};
+
+export const validatePassword = (password: string) => {
+  if (password.length === 0) {
+    return "password is required";
+  }
+  if (password.length < 5) {
+    return "password at leats 5 characters long";
+  }
+};
