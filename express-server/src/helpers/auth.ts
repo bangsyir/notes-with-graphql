b04c1d7366bc696834a93cef26684cf36d66914d
bangsyir <bangsyir@gmail.com>
2023-02-05 22:55:35 +0800

    add clear cookie if unauthorized

--- express-server/src/helpers/auth.ts
+++ express-server/src/helpers/auth.ts
@@ -1,13 +1,16 @@
+import type { Response } from "express";
 import { ErrorResponse } from "../handler/errorHandler";
 import { getUserById } from "../handler/userHandler";
 
-export const Auth = async (userId: number) => {
+export const Auth = async (userId: number, res: Response) => {
   const user = await getUserById(userId);
-  if (!user)
+  if (!user) {
+    res.clearCookie("gassess");
     return ErrorResponse({
       message: "opss you are unautorized",
       status: 401,
       code: "UNAUTORIZED",
     });
+  }
   return user;
 };
