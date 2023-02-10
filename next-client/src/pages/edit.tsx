6e5f4a98b819ff577d20ecd886bbf13bbde27f04
bangsyir <bangsyir@gmail.com>
2023-02-10 12:08:25 +0800

    create edit page

---
+++ next-client/src/pages/edit.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import Link from "next/link";
+
+const EditNote: NextPage = () => {
+  return (
+    <div>
+      <Link href={"/"}>Home</Link>
+      <div>test page</div>
+    </div>
+  );
+};
+
+export default EditNote;
