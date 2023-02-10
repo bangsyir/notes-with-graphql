fc71ea43e387f825c272525b7ca1612489e43ee4
bangsyir <bangsyir@gmail.com>
2023-02-10 12:09:11 +0800

    add custome graphql request for server

--- next-client/src/request/graphqlRequestClient.ts
+++ next-client/src/request/graphqlRequestClient.ts
@@ -4,9 +4,20 @@ const GRAPHQL_ENDPOINT = "http://localhost:4000/graphql" as string;
 const graphqlRequestClient = new GraphQLClient(
   "http://localhost:4000/graphql",
   {
+    // jsonSerializer: {
+    //   parse: JSON.parse,
+    //   stringify: JSON.stringify,
+    // },
     credentials: "include",
     mode: "cors",
   }
 );
+
+export const graphqlRequest = (cookie: string) =>
+  new GraphQLClient("http://localhost:4000/graphql", {
+    headers: {
+      Cookie: cookie,
+    },
+  });
 
 export default graphqlRequestClient;
