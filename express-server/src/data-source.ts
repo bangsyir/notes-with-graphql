d34afe8e004d2606904532ecf4d79fa29d434193
bangsyir <bangsyir@gmail.com>
2023-02-21 14:20:10 +0800

    add js at entity nad migration

--- express-server/src/data-source.ts
+++ express-server/src/data-source.ts
@@ -4,8 +4,8 @@ import { Note } from "./entity/Note";
 export const dataSourceOptions: DataSourceOptions = {
   type: "sqlite",
   database: "db.sqlite",
-  entities: [__dirname + "/entity/*.ts"],
-  migrations: [__dirname + "/migrations/*.ts"],
+  entities: [__dirname + "/entity/*{.js,.ts}"],
+  migrations: [__dirname + "/migrations/*{.js,.ts}"],
 };
 
 export const conn = new DataSource(dataSourceOptions);
