abb070f90c417f98f1efb214dc13480c2bec7cd4
bangsyir <bangsyir@gmail.com>
2023-02-23 19:11:08 +0800

    create image entity

---
+++ express-server/src/entity/Image.ts
@@ -0,0 +1,31 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Note } from "./Note";
+
+@Entity()
+export class Image extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: String, nullable: false })
+  url!: string;
+
+  @ManyToOne(() => Note, (note) => note.images)
+  @JoinColumn({ name: "note_id" })
+  note!: Note;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt!: Date;
+}
