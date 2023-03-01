b54e370d8549d1c166d553f6420c0deb408fe0ad
bangsyir <bangsyir@gmail.com>
2023-03-01 17:48:06 +0800

    create page for server error

---
+++ next-client/src/pages/server-error.tsx
@@ -0,0 +1,10 @@
+export default function ServerError() {
+  return (
+    <div>
+      <div className="grid gap-4 items-center justify-center text-center pt-96">
+        <span className="text-5xl font-bold">500</span>
+        <span className="text-xl">Server Error</span>
+      </div>
+    </div>
+  );
+}
